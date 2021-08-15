ad89583884439e9dc3d2ce0cd5c4bae71780d3a2
midhunvnadh <midhun.vnadh5732@gmail.com>
2021-08-16 00:45:42 +0530

    Finish project'

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,20 @@
 startFlag();
+
+function download(filename, text) {
+  var element = document.createElement("a");
+  element.setAttribute("href", text);
+  element.setAttribute("download", filename);
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+const downloadImage = () => {
+  var canvas = document.getElementById("canvas");
+  var img = canvas.toDataURL("image/png");
+  download("flag.png", img);
+};
